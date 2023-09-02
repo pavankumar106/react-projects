a00cdcc3b5847b1f689ec6714045e4de5b286d8b
Pavankumar Guttedar <pavankumar494748@gmail.com>
2023-09-02 21:55:36 +0530

    update

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -1,21 +1,45 @@
 import React, { useReducer } from "react";
 
+import DigitButton from "./DigitButton";
 import "./app.css";
 
-function reducer(state, action) {}
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand} || ""{payload.digit}`,
+      };
+  }
+}
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, {});
+  const [{ currentOperand, previousOperand, operator }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+
+  // dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } });
 
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">
+          {previousOperand} {operator}
+        </div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
-      <button>÷</button>
+      <DigitButton digit="÷" dispatch={dispatch} />
       <button>1</button>
       <button>2</button>
       <button>3</button>
